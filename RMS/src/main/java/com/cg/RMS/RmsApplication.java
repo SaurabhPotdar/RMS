17f82d46dfa206503a0a6345b537a99871fffad0
Saurabh Potdar <44492033+SaurabhPotdar@users.noreply.github.com>
2019-12-17 09:42:43 +0530

    Spring boot base project

---
+++ RMS/src/main/java/com/cg/RMS/RmsApplication.java
@@ -0,0 +1,14 @@
+package com.cg.RMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmsApplication.class, args);
+		System.out.println("Welcome");
+	}
+
+}
---
+++ RMS/src/main/java/com/cg/dto/User.java
@@ -0,0 +1,8 @@
+package com.cg.dto;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User {
+
+}
